8c6286c4caebb5a7d477350bd0abf8ab2958ffa5
Coslate <coslate@media.ee.ntu.edu.tw>
2024-03-30 00:55:05 +0800

    Complete minHeap setNewValue() and create Map.

--- Lab2/code_practice/03_path_planning.py
+++ Lab2/code_practice/03_path_planning.py
@@ -20,11 +20,28 @@ if __name__ == "__main__":
     m = cv2.cvtColor(m, cv2.COLOR_RGB2GRAY)
     m = m.astype(float) / 255.
     m = 1-cv2.dilate(1-m, np.ones((20,20)))
+    m_copy = m.copy()
     img = img.astype(float)/255.
+    print(f"m.shape = {m.shape}")
+    print(f"img.shape = {img.shape}")
+
+    for i in range(m.shape[0]):
+        for j in range(m.shape[1]):
+            if m[i, j] < 0.5:
+                m[i, j] = 1
+            else:
+                m[i, j] = 0
 
     # Choose Planner
     start=(100,200)
     goal=(380,520)
+
+
+    print(f"m[start[1], start[0]] = {m[start[1], start[0]]}")
+    print(f"m[goal[1], goal[0]] = {m[goal[1], goal[0]]}")
+    print(f"img[start[1], start[0]] = {img[start[1], start[0]]}")
+    print(f"img[goal[1], goal[0]] = {img[goal[1], goal[0]]}")
+
     if args.planner == "a_star":
         from PathPlanning.planner_a_star import PlannerAStar as Planner
         planner = Planner(m, 20)
@@ -40,7 +57,7 @@ if __name__ == "__main__":
     else:
         print("Unknown planner !!")
         exit(0)
-    
+
     print(path)
 
     cv2.circle(img,(start[0],start[1]),5,(0,0,1),3)
@@ -54,7 +71,10 @@ if __name__ == "__main__":
         path = np.array(cubic_spline_2d(path, interval=1))
         for i in range(len(path)-1):
             cv2.line(img, pos_int(path[i]), pos_int(path[i+1]), (1,0,0), 2)
-    
+
     img_ = cv2.flip(img,0)
     cv2.imshow("Path Planning",img_)
+    cv2.imshow("Path Planning img",img)
+    cv2.imshow("Path Planning m_copy",m_copy)
+    cv2.imshow("Path Planning m",m)
     k = cv2.waitKey(0)
--- Lab2/code_practice/PathPlanning/planner_a_star.py
+++ Lab2/code_practice/PathPlanning/planner_a_star.py
@@ -3,42 +3,39 @@ import sys
 sys.path.append("..")
 import PathPlanning.utils as utils
 from PathPlanning.planner import Planner
-from typing import Type, Optional, List
+from typing import Type, Optional, List, Union
 
 class HeapPoint():
-    def __init__(self, position: Optional[tuple[int, int]] = None, value: Optional[int] = None) -> None:
+    def __init__(self, position: Optional[tuple[int, int]] = None, value: Optional[Union[int, float]] = None) -> None:
         self.__position = position
         self.__value    = value
 
     def setPosition(self, position: Optional[tuple[int, int]] = None) -> None:
         self.__position = position
 
-    def setValue(self, value: Optional[int] = None) -> None:
+    def setValue(self, value: Optional[Union[int, float]] = None) -> None:
         self.__value = value
 
-    def setPosValue(self, position: Optional[tuple[int, int]] = None, value: Optional[int] = None) -> None:
+    def setPosValue(self, position: Optional[tuple[int, int]] = None, value: Optional[Union[int, float]] = None) -> None:
         self.__position = position
         self.__value    = value
 
     def getPosition(self) -> Optional[tuple[int, int]]:
         return self.__position
 
-    def getValue(self) -> Optional[int]:
+    def getValue(self) -> Optional[Union[int, float]]:
         return self.__value
 
 
 class MinHeap():
-    def __init__(self, array: Optional[List[tuple[int, int]]] = None) -> None:
+    def __init__(self) -> None:
         self.heap  = []
         self.length = 0
-
-        if array is not None:
-            if len(array) > 0:
-                for item in array:
-                    self.insert(item)
+        self.map_pos2value = {}
 
     def insert(self, item: HeapPoint) -> None:
         self.heap.append(item)
+        self.map_pos2value[item.getPosition()] = self.length
         self.length += 1
         self.__heapUp(self.length-1)
 
@@ -46,16 +43,33 @@ class MinHeap():
         minpt = self.heap[0]
         self.__exchange(0, self.length-1)
         self.heap.pop()
+        self.map_pos2value.pop(minpt.getPosition())
         self.length -= 1
         self.__heapDown(0)
 
         return minpt
+
+    def setNewValue(self, position: tuple[int, int], new_value: Optional[Union[int, float]]) -> None:
+        pos = self.map_pos2value[position]
+        orig_value = self.heap[pos].getValue()
+        self.heap[pos].setValue(new_value)
+
+        if orig_value > new_value:
+            self.__heapUp(pos)
+        elif orig_value < new_value:
+            self.__heapDown(pos)
 
     def printHeap(self) -> None:
         print(f"heap = [ ")
         for i in self.heap:
             print(f"{i.getPosition()}, {i.getValue()} ")
         print(f"]")
+
+    def printMap(self) -> None:
+        sorted_map = {k: v for k, v in sorted(self.map_pos2value.items(), key=lambda item:item[1])}
+        print(f"map_pos2value = ")
+        for items in sorted_map.items():
+            print(f"{items}")
 
     def __heapDown(self, pos: Optional[int]) -> None:
         left_child_idx = pos*2+1
@@ -89,7 +103,9 @@ class MinHeap():
     def __exchange(self, pos1: Optional[int], pos2: Optional[int]) -> None:
         tmp = self.heap[pos1]
         self.heap[pos1] = self.heap[pos2]
+        self.map_pos2value[self.heap[pos1].getPosition()] = pos1
         self.heap[pos2] = tmp
+        self.map_pos2value[self.heap[pos2].getPosition()] = pos2
 
 class PlannerAStar(Planner):
     def __init__(self, m, inter=10):
@@ -104,6 +120,11 @@ class PlannerAStar(Planner):
         self.g = {} # Distance from node to goal
         self.goal_node = None
 
+    def buildAdj(self, img=None) -> None:
+        row = img.shape[0]
+        col = img.shape[1]
+
+
     def planning(self, start=(100,200), goal=(375,520), inter=None, img=None):
         if inter is None:
             inter = self.inter
@@ -115,29 +136,40 @@ class PlannerAStar(Planner):
         self.parent[start] = None
         self.g[start] = 0
         self.h[start] = utils.distance(start, goal)
+
+        minheap = MinHeap()
+        minheap.insert(HeapPoint(start, self.g[start]+self.h[start]))
+
+        '''test heap code'''
+        '''
+        minheap = MinHeap()
+        minheap.insert(HeapPoint((1, 1), 200))
+        minheap.insert(HeapPoint((2, 1), 300))
+        minheap.insert(HeapPoint((2, 43), 600))
+        minheap.insert(HeapPoint((3, 43), 10))
+        minheap.insert(HeapPoint((4, 43), 40))
+        minheap.insert(HeapPoint((5, 43), 5))
+        minheap.insert(HeapPoint((6, 43), 3333))
+        minheap.printHeap()
+        minheap.printMap()
+        print(f"-----")
+        minpt = minheap.extractMin()
+        print(f"minpt = {minpt.getPosition()}, {minpt.getValue()}")
+        minpt = minheap.extractMin()
+        print(f"minpt = {minpt.getPosition()}, {minpt.getValue()}")
+        minheap.printHeap()
+        minheap.printMap()
+        print(f"-----")
+        minheap.setNewValue((6, 43), 6666)
+        minheap.printHeap()
+        minheap.printMap()
+        print(f"-----")
+        minheap.setNewValue((6, 43), 20)
+        minheap.printHeap()
+        minheap.printMap()
+        '''
         while(1):
             # TODO: A Star Algorithm
-            minheap = MinHeap()
-            minheap.insert(HeapPoint(start, self.g[start]+self.h[start]))
-            minheap.printHeap()
-            '''test heap code'''
-            '''
-            minheap = MinHeap()
-            minheap.insert(HeapPoint((1, 1), 200))
-            minheap.insert(HeapPoint((2, 1), 300))
-            minheap.insert(HeapPoint((2, 43), 600))
-            minheap.insert(HeapPoint((3, 43), 10))
-            minheap.insert(HeapPoint((4, 43), 40))
-            minheap.insert(HeapPoint((5, 43), 5))
-            minheap.insert(HeapPoint((6, 43), 3333))
-            minheap.printHeap()
-            print(f"-----")
-            minpt = minheap.extractMin()
-            print(f"minpt = {minpt.getPosition()}, {minpt.getValue()}")
-            minpt = minheap.extractMin()
-            print(f"minpt = {minpt.getPosition()}, {minpt.getValue()}")
-            minheap.printHeap()
-            '''
             break
 
         # Extract path
